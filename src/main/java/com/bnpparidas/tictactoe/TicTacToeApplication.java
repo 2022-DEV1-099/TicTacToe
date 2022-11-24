d41519a284ea4107391a41b0a4cda70419e657a0
Frias <javier.frias@capgemini.com>
2022-11-24 17:31:29 +0100

    add first controller!!!

--- src/main/java/com/bnpparidas/tictactoe/TicTacToeApplication.java
+++ src/main/java/com/bnpparidas/tictactoe/TicTacToeApplication.java
@@ -1,9 +1,13 @@
 package com.bnpparidas.tictactoe;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@Slf4j
 public class TicTacToeApplication {
 
 	public static void main(String[] args) {
